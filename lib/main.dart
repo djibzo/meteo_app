5df3695488a950043aec2fc0fd32a623853be531
djibzo <djibzo90@gmail.com>
2024-02-26 18:10:50 +0000

    ajout page load

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
 
       ),
-        home: const HomeScreen(title: 'Flutter Demo Home Page'),
+        home: const HomeScreen(title: 'Meteo App'),
       );
 
   }
---
+++ lib/screens/loader_weather_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:meteo_app/utils/consts.dart';
+
+class LoaderWeatherScreen extends StatefulWidget {
+  const LoaderWeatherScreen({super.key});
+
+  @override
+  State<LoaderWeatherScreen> createState() => _LoaderWeatherScreenState();
+}
+
+class _LoaderWeatherScreenState extends State<LoaderWeatherScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+      title: Text('Meteo App'),
+      backgroundColor: appliColor,
+      centerTitle: true,
+    ),
+    );
+  }
+}
